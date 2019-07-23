42457cae04580c1005ea17bd5562559232cc3997
Ryan Zheng <960404717@qq.com>
2019-07-23 11:11:55 +0800

    Update keyword_conf_server.php

--- example/config/keyword_conf_server.php
+++ example/config/keyword_conf_server.php
@@ -1,12 +1,12 @@
 <?php
-	header( 'Content-Type:text/html;charset=utf-8');
-	date_default_timezone_set('PRC');
-	// postgresql database
-	define('PG_HOST', '127.0.0.1');
-	define('PG_USER', 'jira_user');
-	define('PG_PASSWD', '***');
-	define('PG_NAME', 'jira_data');
-	define('PG_PORT', 5432);
+    header( 'Content-Type:text/html;charset=utf-8');
+    date_default_timezone_set('PRC');
+    // postgresql database
+    define('PG_HOST', '127.0.0.1');
+    define('PG_USER', 'jira_user');
+    define('PG_PASSWD', '***');
+    define('PG_NAME', 'jira_data');
+    define('PG_PORT', 5432);
     define('LOG_PATH', '/home/jira_log/');
 
     define('DATABASE_TABLE_NAME', 'jira');
